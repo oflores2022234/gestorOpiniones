0a146ba174703e99aefa00121a4cb5cf2f66d408
oflores-2022234 <oflores-2022234@kinal.edu.gt>
2024-03-02 11:38:25 -0600

    new branch, and add folder comments, add model of comments

---
+++ src/comments/comments.routes.js
@@ -0,0 +1,20 @@
+import mongoose, { mongo } from "mongoose";
+
+const CommentSchema = mongoose.Schema({
+    contenido: {
+        type: String,
+        required: true
+    },
+    usuario: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    publicacion: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Publication',
+        required: true
+    }
+});
+
+export default mongoose.model('Comment', CommentSchema);
